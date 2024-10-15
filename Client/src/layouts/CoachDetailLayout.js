5a453ce75d322b041aead71549626af3f8f370ff
thetoannn <naotehtgnoud@gmail.com>
2024-10-15 16:47:54 +0700

    update RelatedCoach

--- Client/src/layouts/CoachDetailLayout.js
+++ Client/src/layouts/CoachDetailLayout.js
@@ -6,26 +6,27 @@ import CoachDetailComponent from '../components/CoachDetailComponent';
 // import RelatedPostsComponent from '../components/RelatedPostsComponent';
 import HomeComponent from '../components/HomeComponent';
 // import "../styles/layouts/CoachDetailLayout.css";
+import RelatedCoachComponent from '../components/RelatedCoachComponent';
 
 const CoachDetailLayout = () => {
   const location = useLocation();
   const { Coach } = location.state; // Extract the Coach data from the state
 
   return (
-    <div className="Coach-detail-layout" style={{ backgroundColor: "#F0F0F0" }}>
+    <div className="court-detail-layout" style={{ backgroundColor: "#F0F0F0" }}>
       {/* Header */}
       <Header />
 
       {/* Main Content */}
-      <div className="main-content">
+      <div className="main-content-court">
 
         <HomeComponent />
-        <div className='main-detail-Coach'>
+        <div className='main-detail-court'>
           {/* Coach Detail Component */}
           <CoachDetailComponent Coach={{ name: "Sân 286 Nguyễn Xiên", price: "100.000", slots: "6", location: "Hà Nội" }} />
 
           {/* Related Posts */}
-          {/* <RelatedPostsComponent /> */}
+          <RelatedCoachComponent />
         </div>
 
       </div>
