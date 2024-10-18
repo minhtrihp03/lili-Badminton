dbc27f620653b98205adfe295bc5ccbba8cce60a
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2024-10-18 20:46:32 +0700

    anh

--- Client/src/components/HomeComponent.js
+++ Client/src/components/HomeComponent.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import '../styles/screens/HomeComponent.css';  // Đảm bảo bạn đã tạo file CSS
-import Banner1 from '../assets/banner-HLV.png';
-import Banner2 from '../assets/banner-2.png';
+import Banner1 from '../assets/banner.png';
+import Banner2 from '../assets/banner1.png';
 
 const HomeComponent = () => {
   const location = useLocation();
